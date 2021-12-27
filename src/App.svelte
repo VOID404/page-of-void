a81f4e554e943b2319284d5a6d04ff4c96fae43e
VOID404 <VOID404@users.noreply.github.com>
2021-12-27 10:03:07 +0100

    Placeholder version (#1)
    * Move to typescript
    * Setup some basic styles
    * Write placeholder content

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,18 @@
-<script>
-	export let name;
+<script lang="ts">
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<h1>Page of VOID</h1>
+	<p>Quite empty for now, but hosted with <a href="https://fleek.co">cool technologies</a></p>
 </main>
 
 <style>
 	main {
 		text-align: center;
 		padding: 1em;
-		max-width: 240px;
 		margin: 0 auto;
 	}
-
 	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
+		font-size: 200%;
 	}
 </style>
